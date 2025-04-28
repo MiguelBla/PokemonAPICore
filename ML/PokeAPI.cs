6c8edd38fdd6875d1355188bbf456136d4e136b3
Carlos Miguel Blanco <Miguel__Blanco@outlook.es>
2025-04-27 20:20:05 -0600

    Add project files.

---
+++ ML/PokeAPI.cs
@@ -0,0 +1,14 @@
+﻿namespace ML
+{
+	public class PokeAPI
+	{
+		
+			public List<Pokemo> Results { get; set; }
+		}
+		public class Pokemo
+		{
+			public string Name { get; set; }
+			public string Url { get; set; }
+		}
+	
+}
---
+++ PL/Controllers/PokemonHTTPClientController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+	public class PokemonHTTPClientController : Controller
+	{
+		public IActionResult GetAll()
+		{
+			return View();
+		}
+
+		public JsonResult GetPokemon(int? pag)
+		{
+			if (pag == null)
+			{
+				pag = 0;
+			}
+
+			ML.Result resultApi = new ML.Result();
+			resultApi.Objects = new List<object>();
+
+			using (var client = new HttpClient())
+			{
+				client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+
+				var resposeTask = client.GetAsync("pokemon?limit=20&offset=" + pag);
+
+
+				resposeTask.Wait();
+
+				var result = resposeTask.Result;
+				var json = result.Content.ReadAsStringAsync();
+
+				if (result.IsSuccessStatusCode)
+				{
+					var readTask = result.Content.ReadAsAsync<ML.PokeAPI>();
+					readTask.Wait();
+					var resultPokeApi = readTask.Result;
+
+					if (resultPokeApi.Results.Count > 0)
+					{
+						foreach (ML.Pokemo pokemonApi in resultPokeApi.Results)
+						{
+							ML.Pokemo pokemon = new ML.Pokemo();
+
+							pokemon.Name = pokemonApi.Name;
+							string[] numpokemon = pokemonApi.Url.Split('/');
+							pokemon.Url = numpokemon[numpokemon.Length - 2];
+
+							resultApi.Objects.Add(pokemon);
+						}
+						resultApi.Correct = true;
+
+					}
+				}
+				else
+				{
+					resultApi.Correct = false;
+					resultApi.ErrorMessage = "No se encontraron registros";
+				}
+
+
+			}
+			return Json(resultApi);
+		}
+	}
+}
